73d9a1ab631e0df49613b65e96d5ff2f98adc73c
TonyRegy <tonyregy@gmail.com>
2020-05-22 15:27:23 +0530

    daily wage

---
+++ emplyDailyWage.java
@@ -0,0 +1,10 @@
+public class emplyDailyWage{
+
+	public static void main(String[] args){
+	int RATE_PER_HOUR=20;
+	int HOURS_PER_DAY=8;
+	int p;
+	p=RATE_PER_HOUR*HOURS_PER_DAY;
+	System.out.println("Daiy wage is:"+p);
+}
+}
